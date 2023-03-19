ec02a7a1876a8b2db49b5415b983f199f3746616
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-03-19 01:53:33 +0100

    fix

--- tests_integration/test_warmup_cosine.py
+++ tests_integration/test_warmup_cosine.py
@@ -28,3 +28,27 @@ class TestWarmupCosine(unittest.TestCase):
         total_steps = 10
         values = [sched.get_value(i, total_steps) for i in range(total_steps)]
         assertIsClose(self, [0.2, 0.4, 0.6, 0.8, 1.0, 0.933, 0.75, 0.5, 0.25, 0.067], values)
+
+    def test_percent_equal_to_step(self):
+        percent = ks.SequentialPercentSchedule([
+            ks.SequentialPercentScheduleConfig(
+                end_percent=0.1,
+                schedule=ks.LinearIncreasingSchedule(exclude_first=True, exclude_last=True),
+            ),
+            ks.SequentialPercentScheduleConfig(
+                schedule=ks.CosineDecreasingSchedule(exclude_last=True),
+            ),
+        ])
+        step = ks.SequentialStepSchedule([
+            ks.SequentialStepScheduleConfig(
+                end_step=19,
+                schedule=ks.LinearIncreasingSchedule(exclude_first=True, exclude_last=True),
+            ),
+            ks.SequentialStepScheduleConfig(
+                schedule=ks.CosineDecreasingSchedule(exclude_last=True),
+            ),
+        ])
+        total_steps = 194
+        percent_values = [percent.get_value(i, total_steps) for i in range(total_steps)]
+        step_values = [step.get_value(i, total_steps) for i in range(total_steps)]
+        self.assertEqual(percent_values, step_values)
