98b922ca05e293a7cbd2dfbd5b74abe690fe83ed
Benedikt Alkin <39829911+BenediktAlkin@users.noreply.github.com>
2023-07-14 14:16:17 +0200

    Revert "min_value"
    This reverts commit 8d95db66e79f66f2998ecc6378099852f85bcfd3.

--- kappaschedules/schedules/base/schedule_base.py
+++ kappaschedules/schedules/base/schedule_base.py
@@ -1,15 +1,11 @@
 class ScheduleBase:
-    def __init__(self, min_value=None):
-        self.min_value = min_value
+    def __init__(self):
         # check that correct method is overwritten
         assert type(self).get_value == ScheduleBase.get_value
 
     def get_value(self, step: int, total_steps: int, abs_step: int = None) -> float:
         assert 0 <= step < total_steps, f"0 <= step < total_steps (step={step} total_steps={total_steps})"
-        value = self._get_value(step, total_steps, abs_step)
-        if self.min_value is not None and value < self.min_value:
-            return self.min_value
-        return value
+        return self._get_value(step, total_steps, abs_step)
 
     def _get_value(self, step: int, total_steps: int, abs_step: int = None) -> float:
         raise NotImplementedError
